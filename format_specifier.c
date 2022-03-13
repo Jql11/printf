5e4e9c855e6f3ae5a9f09c7ad554063b952dbabe
jql11 <4325@holbertonschool.com>
2022-03-12 22:45:15 -0800

    update with no print_int function

--- format_specifier.c
+++ format_specifier.c
@@ -39,25 +39,6 @@ int print_percent(__attribute__((unused))va_list ap)
 	_putchar('%');
 	return (1);
 }
-/**
-  *print_signed_num: print signed number;
-  *@ap: arguments
-  */
-int print_signed_num(va_list ap)
-{
-	int n;
-
-	n = va_arg(ap, int);
-	if (n < 0)
-	{
-		_putchar(n * -1);
-	}
-	else
-		_putchar(n);
-
-	return (1);
-
-}
 
 
 
