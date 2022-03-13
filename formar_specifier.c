ea0b44e2bfa778b4c02c564b67199bbf72c9319f
jql11 <4325@holbertonschool.com>
2022-03-12 22:51:34 -0800

    fix

---
+++ formar_specifier.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  *print_char - print char
+  *@args: arguments
+  */
+int print_char(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (1);
+}
+/**
+  *print_str - print string
+  *@ap: arguments
+  */
+int print_str(va_list ap)
+{
+	unsigned int i = 0;
+	char *str;
+
+	str = va_arg(ap, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+
+	while(str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+/**
+  *print_percent: print percent
+  *@ap:arguments
+  */
+int print_percent(__attribute__((unused))va_list ap)
+{
+	_putchar('%');
+	return (1);
+}
+
