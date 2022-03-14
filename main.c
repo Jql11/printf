7fc75e7ef3d73ee2f976688111fca114637fc375
jql11 <4325@holbertonschool.com>
2022-03-13 20:09:05 -0700

    add u intialize

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ int main(void)
     int len;
     int len2;
     unsigned int ui;
+
+    ui = (unsigned int)INT_MAX + 1024;
     
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
