af6d2ad1dcec166c953fdd4ecec3de128d1d99ad
jql11 <4325@holbertonschool.com>
2022-03-14 05:22:20 -0700

    fix betty

--- print_binary.c
+++ print_binary.c
@@ -5,19 +5,19 @@
   *@n: integer
   *Return: binary number
   */
-int convert_binary(int n) 
+int convert_binary(int n)
 {
-  int bin = 0;
-  int rem, i = 1;
+	int bin = 0;
+	int rem, i = 1;
 
-  while (n!=0) {
-    rem = n % 2;
-    n /= 2;
-    bin += rem * i;
-    i *= 10;
-  }
-
-  return (bin);
+	while (n != 0)
+	{
+		rem = n % 2;
+		n /= 2;
+		bin += rem * i;
+		i *= 10;
+	}
+	return (bin);
 }
 /**
   *print_binary - print binary
