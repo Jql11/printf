8489ebbb93facc63283808aeb565fb377633dbe1
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-13 19:08:58 -0700

    build: update main.h with function prototype

--- main.h
+++ main.h
@@ -21,7 +21,7 @@ int print_percent(va_list args);
 int print_int(va_list ap);
 int print_number(int n);
 int _strlen(char *s);
-//int _puts(char *str);
-// int print_rev(va_list ap)
+/* int _puts(char *str); */
+/* int print_rev(va_list ap) */
 
 #endif /* MAIN_H */
