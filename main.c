d5cc71e02e8fcfb0f17ca4f0a56673770702304e
jql11 <4325@holbertonschool.com>
2022-03-13 20:07:04 -0700

    add file

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    int len2;
+    unsigned int ui;
+    
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Percent:[%%]\n");
+    len = _printf("Percent:[%%]\n");
+    printf("Character:[%c]\n", 'H');
+    _printf("Character:[%c]\n", 'H');
+    printf("String:[%s]\n", "I am a string !");
+    _printf("String:[%s]\n", "I am a string !");
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+
+    return (0);
+}
+
