10b59b94ea21a627feb45255891f3f3e1a6313b5
jql11 <4325@holbertonschool.com>
2022-03-14 04:50:21 -0700

    delete length_digit function

--- print_binary.c
+++ print_binary.c
@@ -2,29 +2,13 @@
 #include <math.h>
 #include <stdarg.h>
 /**
-*length_digit - number of digit
-*@n: integer
-* Return: Always 0.
-*/
-int length_digit(int n)
-{
-	int length = 0;
-
-	while (n != 0)
-	{
-		n = n / 10;
-		length++;
-	}
-	return (length);
-}
-/**
   *convert_binary - print binary
   *@n: integer
   *Return: binary number
   */
 int convert_binary(int n) 
 {
-  long long bin = 0;
+  long bin = 0;
   int rem, i = 1;
 
   while (n!=0) {
