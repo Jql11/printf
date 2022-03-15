ecc2cb4ed44834e9c874d203ca7cff21eee080bf
jql11 <4325@holbertonschool.com>
2022-03-14 22:04:45 -0700

    betty

--- print_binary.c
+++ print_binary.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include "main.h"
 /**
-  *convert_binary - print binary
-  *@n: integer
-  *Return: binary number
+  *print_binary - print binary
+  *@ap: integer
+  *Return: count binary number
   */
 int print_binary(va_list ap)
 {
 	unsigned int n;
 	int rem[32], i = 0, j;
+
 	n = va_arg(ap, unsigned int);
 	if (n == 0)
 	{
-		_putchar(‘0’);
+		_putchar('0');
 		return (1);
 	}
 	while (n > 0)
 	{
-			rem[i] = n % 2;
-			n /= 2;
-			i++;
+		rem[i] = n % 2;
+		n /= 2;
+		i++;
 	}
 	j = i - 1;
 	while (j >= 0)
 	{
-		_putchar(rem[j] + ‘0’);
+		_putchar(rem[j] + '0');
 		j--;
 	}
 	return (i);
