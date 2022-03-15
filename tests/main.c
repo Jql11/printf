45f013438aee724ed292402b8070130a269205f5
Chiara Caprasi <caprasichiara@gmail.com>
2022-03-14 21:36:30 -0700

    tests: move main.c out of main folder

--- tests/main.c
+++ tests/main.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,7 +9,25 @@
  */
 int main(void)
 {
-	char 
-    _printf("%R\n", str);
+    int len;
+    int len2;
+    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+
+    _printf("%R", s);
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+     _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+     _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+     len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+/*    _printf("Unknown:[%r]\n"); */
     return (0);
 }
